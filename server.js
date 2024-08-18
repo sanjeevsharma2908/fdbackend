ee3d8798cc8cd3335fe0dc242d6dda0621c29380
Sanjeev Sharma <69752064+sanjeevsharma2908@users.noreply.github.com>
2024-08-18 18:11:46 +0530

    user-route created

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
+import 'dotenv/config'
+
 
 
 
@@ -21,6 +24,8 @@ connectDB();
 //API endpoints
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
+
 
 
 app.get("/", (req,res) =>{
